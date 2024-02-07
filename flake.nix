56f0f5f288be5996b57191c6e76b056e198d5ad7
Robbe Van Petegem <git@robbevp.be>
2024-02-07 18:38:31 +0100

    Add package as output to flake

--- flake.nix
+++ flake.nix
@@ -9,6 +9,11 @@
         nixpkgs.follows = "nixpkgs";
       };
     };
+    crane = {
+      # Use a fork until https://github.com/ipetkov/crane/pull/358 is merged
+      url = "github:shimunn/crane?ref=tauri-package";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
@@ -23,8 +28,77 @@
             inputs.devshell.overlays.default
           ];
         };
+        craneLib = inputs.crane.lib.${system};
+
+        frontend = pkgs.mkYarnPackage {
+          pname = "erasmus-frontend";
+          version = "unstable";
+
+          src = pkgs.lib.cleanSource ./src;
+
+          packageJSON = ./src/package.json;
+          yarnLock = ./yarn.lock;
+
+          buildPhase = ''
+            yarn run build
+          '';
+
+          installPhase = ''
+            cp -r deps/frontend/dist $out
+          '';
+
+          distPhase = "true";
+        };
+
+        tauriArgs = {
+          src = pkgs.lib.cleanSourceWith {
+            src = ./src-tauri; # The original, unfiltered source
+            filter = path: type:
+              (pkgs.lib.hasSuffix "\.html" path) ||
+              (pkgs.lib.hasSuffix "\.scss" path) ||
+              (pkgs.lib.hasSuffix "\.css" path) ||
+              (pkgs.lib.hasSuffix "\.json" path) ||
+              (pkgs.lib.hasSuffix "\.png" path) ||
+              # Example of a folder for images, icons, etc
+              (pkgs.lib.hasInfix "/assets/" path) ||
+              # Default filter from crane (allow .rs files)
+              (craneLib.filterCargoSources path type)
+            ;
+          };
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          buildInputs = [
+            # Add additional build inputs here
+          ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+            pkgs.webkitgtk
+            pkgs.gtk3
+            pkgs.cairo
+            pkgs.gdk-pixbuf
+            pkgs.glib
+            pkgs.dbus
+          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            # Additional darwin specific inputs can be set here
+            pkgs.libiconv
+            pkgs.darwin.apple_sdk.frameworks.AppKit
+            pkgs.darwin.apple_sdk.frameworks.WebKit
+          ];
+        };
+
+        # Build *just* the cargo dependencies, so we can reuse
+        # all of that work (e.g. via cachix) when running in CI
+        cargoArtifacts = craneLib.buildDepsOnly (tauriArgs);
+
       in
       {
+        packages = rec {
+          default = erasmus;
+          erasmus = craneLib.buildTauriPackage (tauriArgs // {
+            pname = "erasmus";
+            version = "unstable";
+            inherit cargoArtifacts;
+            tauriConfigPath = ./src-tauri/tauri.conf.json;
+            tauriDistDir = frontend;
+          });
+        };
         devShells = rec {
           default = erasmus;
           erasmus = pkgs.devshell.mkShell {
