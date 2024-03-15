b62c96c7d02fb92159e30f43b9bec3ebac9973d4
finlaydegrauwe <finlaydegrauwe@gmail.com>
2024-03-15 17:22:36 +0100

    Delete rfid_test folder with test

--- rfid_test/target/debug/deps/rfid_test.d
+++
@@ -1,5 +0,0 @@
-D:\Documents\Websites\Phygital\erasmus\rfid_test\target\debug\deps\rfid_test.exe: src\main.rs
-
-D:\Documents\Websites\Phygital\erasmus\rfid_test\target\debug\deps\rfid_test.d: src\main.rs
-
-src\main.rs:
--- rfid_test/target/debug/rfid_test.d
+++
@@ -1,1 +0,0 @@
-D:\Documents\Websites\Phygital\erasmus\rfid_test\target\debug\rfid_test.exe: D:\Documents\Websites\Phygital\erasmus\rfid_test\src\main.rs
