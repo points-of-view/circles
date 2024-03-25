8427fa6f143b4e5f067f7bd3dceab0cec165a6f0
Robbe Van Petegem <git@robbevp.be>
2024-03-25 08:48:51 +0100

    Fix build script

--- reader/build.sh
+++ reader/build.sh
@@ -8,4 +8,4 @@ echo "Main-Class: reader.PrintRFIDReader.PrintRFIDTags" > $SCRIPT_DIR/dist/META-
 javac -cp $SCRIPT_DIR/vendor/zebra/lib/Symbol.RFID.API3.jar -d $SCRIPT_DIR/dist $SCRIPT_DIR/PrintRFIDReader/PrintRFIDTags.java
 cp -r $SCRIPT_DIR/vendor $SCRIPT_DIR/dist/vendor
 cd $SCRIPT_DIR/dist
-jar -cmvf META-INF/MANIFEST.MF reader.jar reader/PrintRFIDReader/PrintRFIDTags.class  >/dev/null
+jar -cmvf META-INF/MANIFEST.MF reader.jar reader/PrintRFIDReader/*.class  >/dev/null
