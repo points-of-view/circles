b9fa7af6ee59d231fc90c3c9550920c84a03be93
Robbe Van Petegem <git@robbevp.be>
2024-03-26 18:01:47 +0100

    Update paths for windows

--- main/src/reader.rs
+++ main/src/reader.rs
@@ -1,18 +1,14 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, env};
 use tauri::{
-    api::process::{Command, CommandChild, CommandEvent},
-    AppHandle,
+    api::process::{Command, CommandChild, CommandEvent}, async_runtime::{channel, Receiver}, AppHandle, Manager
 };
 
-const JAR_PATH: &str = "/_up_/reader/dist/reader.jar";
-const VENDOR_JAR: &str = "/Symbol.RFID.API3.jar";
-const LIBRARY_PATH: &str = "/_up_/reader/dist/vendor/zebra/lib";
 const MAIN_JAVA_CLASS: &str = "reader.PrintRFIDReader.PrintRFIDTags";
 
 // NOTE: We should create a mock method for this, so that development can work without a physical reader
 pub fn spawn_reader(
-    app: AppHandle,
-) -> (tauri::async_runtime::Receiver<CommandEvent>, CommandChild) {
+    app: &AppHandle,
+) -> (Receiver<CommandEvent>, CommandChild) {
     let resource_path = app
         .path_resolver()
         .resource_dir()
@@ -22,6 +18,18 @@ pub fn spawn_reader(
     if env::var("MOCK_RFID_READER").is_ok() {
         return spawn_mock_command();
     }
+
+    let command = if cfg!(all(target_os = "linux", target_arch = "x86_64")) {
+        linux_command(resource_dir)
+    } else if cfg!(target_os = "windows") {
+        windows_command(resource_dir)
+    } else {
+        panic!(
+            "`spawn_reader` can not be called on {arch}-{os}. The java SDK and underlying native libraries are only supported on windows and linux. Use `MOCK_RFID_READER=1` to mock the reader output",
+            arch = env::consts::ARCH,
+            os = env::consts::OS
+        )
+    };
 
     command.spawn().unwrap()
 }
@@ -37,25 +45,27 @@ fn spawn_mock_command() -> (Receiver<CommandEvent>, CommandChild) {
 }
 
 fn windows_command(resource_dir: &str) -> Command {
+    let vendor_jar = r"\_up_\reader\dist\vendor\zebra\lib\Symbol.RFID.API3.jar";
+    let reader_jar = r"\_up_\reader\dist\reader.jar";
+
     Command::new("java").args([
         "-cp",
         &format!(
-            "{path}{lib_path}{vendor_jar};{path}{jar_path}",
+            "{path}{vendor_jar};{path}{reader_jar}",
             path = resource_dir,
-            lib_path = LIBRARY_PATH,
-            vendor_jar = VENDOR_JAR,
-            jar_path = JAR_PATH
+            vendor_jar = vendor_jar,
+            reader_jar = reader_jar
         ),
         MAIN_JAVA_CLASS,
     ])
 }
 
-#[cfg(not(target_os = "windows"))]
-fn unix_command(resource_dir: &str) -> Command {
+fn linux_command(resource_dir: &str) -> Command {
+    let library_path = "/_up_/reader/dist/vendor/zebra/lib";
     let vendor_path = format!(
         "{path}{lib_path}/x86_64",
         path = resource_dir,
-        lib_path = LIBRARY_PATH
+        lib_path = library_path
     );
     let envs: HashMap<String, String> =
         HashMap::from([("LD_LIBRARY_PATH".into(), vendor_path.clone())]);
@@ -66,9 +76,9 @@ fn unix_command(resource_dir: &str) -> Command {
         &format!(
             "{path}{lib_path}{vendor_jar}:{path}{jar_path}",
             path = resource_dir,
-            lib_path = LIBRARY_PATH,
-            vendor_jar = VENDOR_JAR,
-            jar_path = JAR_PATH
+            lib_path = library_path,
+            vendor_jar = "/Symbol.RFID.API3.jar",
+            jar_path = "/_up_/reader/dist/reader.jar"
         ),
         MAIN_JAVA_CLASS,
     ])
