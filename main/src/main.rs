cd5c14591d7ddef6ed9176a16df6eb3a2037f4c6
Robbe Van Petegem <git@robbevp.be>
2024-03-26 21:24:04 +0100

    Remove debugging code

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,9 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use erasmus::{reader::spawn_reader, GlobalState};
+use erasmus::GlobalState;
 use std::fs;
-use tauri::{api::process::CommandEvent, Manager};
+use tauri::Manager;
 
 #[tauri::command]
 fn select_project(
@@ -12,7 +12,7 @@ fn select_project(
     project_key: String,
 ) -> Result<(), String> {
     state.select_project(project_key)?;
-    state.start_reading(app_handle)
+    state.start_reading(&app_handle)
 }
 
 #[tauri::command]
@@ -28,31 +28,8 @@ fn main() {
                 .app_data_dir()
                 .expect("Error while getting `app_data_dir`");
 
-            let resource_dir = app
-                .path_resolver()
-                .resource_dir()
-                .expect("Error while getting `resource_dir`");
-
-            println!(
-                "data_dir: {:?}, resource dir: {:?}",
-                &data_dir, resource_dir
-            );
-
             // Make sure the data_dir exists
             fs::create_dir_all(&data_dir)?;
-
-            let (mut rx, _child) = spawn_reader(app.handle());
-            tauri::async_runtime::spawn(async move {
-                while let Some(event) = rx.recv().await {
-                    match event {
-                        CommandEvent::Stderr(line) => println!("stderr: {}", line),
-                        CommandEvent::Stdout(line) => println!("stdout: {}", line),
-                        CommandEvent::Error(ee) => println!("error: {}", ee),
-                        CommandEvent::Terminated(_) => println!("Terminated!"),
-                        _ => todo!(),
-                    }
-                }
-            });
 
             data_dir.push("erasmus_db.sqlite");
             let state = GlobalState::build(data_dir)?;
