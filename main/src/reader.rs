b3dd4aa96652967904a80635dbe4079431484449
Robbe Van Petegem <git@robbevp.be>
2024-03-26 21:23:24 +0100

    Fix paths for resources

--- main/src/reader.rs
+++ main/src/reader.rs
@@ -1,28 +1,30 @@
-use std::{collections::HashMap, env};
+use dunce::simplified;
+use std::{collections::HashMap, env, path::PathBuf};
 use tauri::{
-    api::process::{Command, CommandChild, CommandEvent}, async_runtime::{channel, Receiver}, AppHandle, Manager
+    api::process::{Command, CommandChild, CommandEvent},
+    async_runtime::{channel, Receiver},
+    AppHandle,
 };
 
 const MAIN_JAVA_CLASS: &str = "reader.PrintRFIDReader.PrintRFIDTags";
 
 // NOTE: We should create a mock method for this, so that development can work without a physical reader
-pub fn spawn_reader(
-    app: &AppHandle,
+pub fn spawn_reader<R: tauri::Runtime>(
+    app: &AppHandle<R>,
 ) -> (Receiver<CommandEvent>, CommandChild) {
     let resource_path = app
         .path_resolver()
         .resource_dir()
         .expect("Error while getting `resource_dir`");
-    let resource_dir = resource_path.to_str().unwrap();
 
     if env::var("MOCK_RFID_READER").is_ok() {
         return spawn_mock_command();
     }
 
     let command = if cfg!(all(target_os = "linux", target_arch = "x86_64")) {
-        linux_command(resource_dir)
+        linux_command(resource_path)
     } else if cfg!(target_os = "windows") {
-        windows_command(resource_dir)
+        windows_command(resource_path)
     } else {
         panic!(
             "`spawn_reader` can not be called on {arch}-{os}. The java SDK and underlying native libraries are only supported on windows and linux. Use `MOCK_RFID_READER=1` to mock the reader output",
@@ -40,45 +42,46 @@ fn spawn_mock_command() -> (Receiver<CommandEvent>, CommandChild) {
     // Instead we can use this channel to send some generated data
     // TODO: Our mock implementation is not yet sending any messages. We should generate some random responses
     let (_tx, rx) = channel(1);
-    
+
     (rx, child)
 }
 
-fn windows_command(resource_dir: &str) -> Command {
-    let vendor_jar = r"\_up_\reader\dist\vendor\zebra\lib\Symbol.RFID.API3.jar";
-    let reader_jar = r"\_up_\reader\dist\reader.jar";
+fn windows_command(resource_path: PathBuf) -> Command {
+    let vendor_jar = resource_path.join(r"_up_\reader\dist\vendor\zebra\lib\Symbol.RFID.API3.jar");
+    let reader_jar = resource_path.join(r"_up_\reader\dist\reader.jar");
+
+    // We need to simplify these paths, and remove any optional UNC prefix (`//?`), since java can't resolve this
+    let simplified_vendor_jar = simplified(&vendor_jar.as_path());
+    let simplified_reader_jar = simplified(&reader_jar.as_path());
 
     Command::new("java").args([
         "-cp",
         &format!(
-            "{path}{vendor_jar};{path}{reader_jar}",
-            path = resource_dir,
-            vendor_jar = vendor_jar,
-            reader_jar = reader_jar
+            "{};{}",
+            simplified_vendor_jar.to_string_lossy(),
+            simplified_reader_jar.to_string_lossy()
         ),
         MAIN_JAVA_CLASS,
     ])
 }
 
-fn linux_command(resource_dir: &str) -> Command {
-    let library_path = "/_up_/reader/dist/vendor/zebra/lib";
-    let vendor_path = format!(
-        "{path}{lib_path}/x86_64",
-        path = resource_dir,
-        lib_path = library_path
-    );
-    let envs: HashMap<String, String> =
-        HashMap::from([("LD_LIBRARY_PATH".into(), vendor_path.clone())]);
+fn linux_command(resource_path: PathBuf) -> Command {
+    let library_path = resource_path.join("_up_/reader/dist/vendor/zebra/lib/x86_64");
+    let vendor_jar = resource_path.join("_up_/reader/dist/vendor/zebra/lib/Symbol.RFID.API3.jar");
+    let reader_jar = resource_path.join("_up_/reader/dist/reader.jar");
+
+    let envs: HashMap<String, String> = HashMap::from([(
+        "LD_LIBRARY_PATH".into(),
+        library_path.to_string_lossy().into_owned(),
+    )]);
 
     Command::new("java").envs(envs).args([
-        &format!("-Djava.library.path='{}'", vendor_path),
+        &format!("-Djava.library.path='{}'", library_path.to_string_lossy()),
         "-cp",
         &format!(
-            "{path}{lib_path}{vendor_jar}:{path}{jar_path}",
-            path = resource_dir,
-            lib_path = library_path,
-            vendor_jar = "/Symbol.RFID.API3.jar",
-            jar_path = "/_up_/reader/dist/reader.jar"
+            "{}:{}",
+            vendor_jar.to_string_lossy(),
+            reader_jar.to_string_lossy()
         ),
         MAIN_JAVA_CLASS,
     ])
