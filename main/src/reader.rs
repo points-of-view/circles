3848b4b9152eb080faedd978491896799db4d95c
Robbe Van Petegem <git@robbevp.be>
2024-03-28 19:08:19 +0100

    Read and parse tags coming through stdout

--- main/src/reader.rs
+++ main/src/reader.rs
@@ -7,6 +7,30 @@ use tauri::{
     async_runtime::{channel, spawn, Receiver},
 };
 
+use crate::tags::{create_mock_tag, Tag};
+
+pub fn handle_reader_event(event: CommandEvent) {
+    match event {
+        CommandEvent::Stderr(_) => todo!(),
+        CommandEvent::Stdout(line) => handle_reader_stdout(line),
+        CommandEvent::Error(_) => todo!(),
+        CommandEvent::Terminated(_) => todo!(),
+        _ => todo!(),
+    }
+}
+
+fn handle_reader_stdout(line: String) {
+    match Tag::from_reader(line) {
+        Ok(tag) => println!("{:?}", tag),
+        Err(err) => {
+            // We print faulty tags in development (so we can learn from them)
+            // In production these get ignored
+            #[cfg(debug_assertions)]
+            println!("{:?}", err)
+        }
+    }
+}
+
 const MAIN_JAVA_CLASS: &str = "reader.PrintRFIDReader.PrintRFIDTags";
 
 // NOTE: We spawn a slightly different command for different OSes/architectures
