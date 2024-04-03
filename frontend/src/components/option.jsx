dfb143485a95685a8d4fdc0085310f6bbe227a21
finlaydegrauwe <finlaydegrauwe@gmail.com>
2024-04-03 17:13:29 +0200

    Linting

--- frontend/src/components/option.jsx
+++ frontend/src/components/option.jsx
@@ -9,12 +9,12 @@ export default function Option({ label, amount = 0, option }) {
       }}
     >
       <div className="option__amount">{amount}</div>
-      <div className="option__content" >
+      <div className="option__content">
         <div className="option__label">{label}</div>
         {amount != 0 && (
           <div className="option__figure-container">
             {[...Array(amount)].map((value, index) => (
-              <Shape key={index} shape={"figure"}/>
+              <Shape key={index} shape={"figure"} />
             ))}
           </div>
         )}
