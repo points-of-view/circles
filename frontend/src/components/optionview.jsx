6e97795dee983947327ef2c663ca5745542ff0e3
finlaydegrauwe <finlaydegrauwe@gmail.com>
2024-04-04 12:12:52 +0200

    Positioning of circles in flexbox

--- frontend/src/components/optionview.jsx
+++ frontend/src/components/optionview.jsx
@@ -6,6 +6,8 @@ export default function OptionView() {
             <Option label={"Optie 1"} amount={25} />
             <Option label={"Optie 2"} amount={13} />
             <Option label={"Optie 3"} amount={0} />
+            {/* <Option label={"Optie 4"} amount={1} /> */}
+            {/* <Option label={"Optie 5"} amount={8} /> */}
         </div>
     )
 }
