0cb6d747fe9aa20f262fad4d23eeca47028fdfec
finlaydegrauwe <finlaydegrauwe@gmail.com>
2024-04-04 12:13:45 +0200

    Linting

--- frontend/src/components/optionview.jsx
+++ frontend/src/components/optionview.jsx
@@ -1,13 +1,11 @@
 import Option from "./option";
 
 export default function OptionView() {
-    return (
-        <div className="optionview">
-            <Option label={"Optie 1"} amount={25} />
-            <Option label={"Optie 2"} amount={13} />
-            <Option label={"Optie 3"} amount={0} />
-            {/* <Option label={"Optie 4"} amount={1} /> */}
-            {/* <Option label={"Optie 5"} amount={8} /> */}
-        </div>
-    )
+  return (
+    <div className="optionview">
+      <Option label={"Optie 1"} amount={25} />
+      <Option label={"Optie 2"} amount={13} />
+      <Option label={"Optie 3"} amount={0} />
+    </div>
+  );
 }
