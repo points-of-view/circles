6a6c8752d7f7ac70383eb09ae66ab02889bcd7d3
finlaydegrauwe <finlaydegrauwe@gmail.com>
2024-04-05 17:07:46 +0200

    Changed order and removed timer

--- frontend/src/components/interaction-screen.jsx
+++ frontend/src/components/interaction-screen.jsx
@@ -3,9 +3,8 @@ export function InteractionScreen() {
     <div className="interaction-screen">
       <div className="interaction-screen__title"></div>
       <div className="interaction-screen__description"></div>
-      <div className="interaction-screen__timer"></div>
-      <div className="interaction-screen__logo"></div>
       <div className="interaction-screen__theme"></div>
+      <div className="interaction-screen__logo"></div>
     </div>
   );
 }
