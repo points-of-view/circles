801cb091308343ab4dc5862dc23e238353f7107d
finlaydegrauwe <finlaydegrauwe@gmail.com>
2024-04-23 21:25:32 +0200

    Display steps in loop

--- frontend/src/components/optionsview.jsx
+++ frontend/src/components/optionsview.jsx
@@ -1,24 +1,29 @@
 import Option from "./option";
 
-export default function OptionsView({ options, chosenOption }) {
+export default function OptionsView({
+  options,
+  showDescriptionLayout = false,
+}) {
   return (
     <div className="options-view interaction-screen__option-view">
-      {chosenOption ?
-        <Option
-          key={chosenOption}
-          className="options-view__option"
-          label={chosenOption}
-          amount={0}
-          big
-        />
-      : options.map((key) => (
+      {Array.isArray(options) ? (
+        options.map((key) => (
           <Option
             key={key}
             className="options-view__option"
             label={key}
             amount={0}
           />
-        ))}
+        ))
+      ) : (
+        <Option
+          key={options}
+          className="options-view__option"
+          label={options}
+          showDescriptionLayout={showDescriptionLayout}
+          big
+        />
+      )}
     </div>
   );
 }
