9eb81ce0e163d10e840287f23baf838efe8f3842
finlaydegrauwe <43783119+finlaydegrauwe@users.noreply.github.com>
2024-05-07 16:15:06 +0200

    Add Slovenian copy to index.js (#193)

--- projects/index.js
+++ projects/index.js
@@ -1,5 +1,6 @@
 import test from "./test.json";
 import phase1bel from "./phase1bel.json";
+import phase1slov from "./phase1slov.json";
 
-const projects = { test, phase1bel };
+const projects = { test, phase1bel, phase1slov };
 export default projects;
