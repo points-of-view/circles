6574dbb32f7931cd3502e0bf0d4e3148977aa0c9
finlaydegrauwe <43783119+finlaydegrauwe@users.noreply.github.com>
2024-05-23 09:04:00 +0200

    Add Portugese copy (#216)

--- projects/index.js
+++ projects/index.js
@@ -1,6 +1,7 @@
 import test from "./test.json";
 import phase1bel from "./phase1bel.json";
 import phase1slov from "./phase1slov.json";
+import phase1port from "./phase1port.json";
 
-const projects = { test, phase1bel, phase1slov };
+const projects = { test, phase1bel, phase1slov, phase1port };
 export default projects;
