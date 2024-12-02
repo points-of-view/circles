1ce68226762e2a110efb8d61c64cada34a02e1ff
finlaydegrauwe <43783119+finlaydegrauwe@users.noreply.github.com>
2024-12-02 14:15:43 +0100

    Update and add English copy (#296)
    * Update copy key and instruction
    
    * Add English project with copy

--- projects/index.js
+++ projects/index.js
@@ -2,6 +2,7 @@ import test from "./test.json";
 import phase1bel from "./phase1bel.json";
 import phase1slov from "./phase1slov.json";
 import phase1port from "./phase1port.json";
+import phase1eng from "./phase1eng.json";
 
-const projects = { test, phase1bel, phase1slov, phase1port };
+const projects = { test, phase1bel, phase1slov, phase1port, phase1eng };
 export default projects;
