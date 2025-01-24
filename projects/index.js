e8d951d09414bc655d819e4b724a62b0fa14ec02
finlaydegrauwe <43783119+finlaydegrauwe@users.noreply.github.com>
2025-01-24 11:24:31 +0100

    Input Slovenian copy (#310)

--- projects/index.js
+++ projects/index.js
@@ -3,6 +3,7 @@ import phase1bel from "./phase1bel.json";
 import phase1slov from "./phase1slov.json";
 import phase1port from "./phase1port.json";
 import phase1eng from "./phase1eng.json";
+import bibi from "./bibi.json";
 
-const projects = { test, phase1bel, phase1slov, phase1port, phase1eng };
+const projects = { test, phase1bel, phase1slov, phase1port, phase1eng, bibi };
 export default projects;
