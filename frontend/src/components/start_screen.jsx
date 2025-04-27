e53c8700953ffc3b3e140289fe4c0ef45a6b7fa3
finlaydegrauwe <finlaydegrauwe@gmail.com>
2025-04-27 16:45:43 +0200

    Code start screen card

--- frontend/src/components/start_screen.jsx
+++ frontend/src/components/start_screen.jsx
@@ -12,8 +12,10 @@ const STATES = {
   done: "DONE",
 };
 
-export function StartScreen({ setDarkMode, toggleFullScreen, projects }) {
-  const [error, setError] = useState(null);
+export function StartScreen({ setProjectKey, setDarkMode, toggleFullScreen, projects }) {
+  const [error, setError] = useState(null); //nog iets mee doen
+  const [viewStartCard, setViewStartCard] = useState(false)
+  const [selectedProject,setSelectedProject] = useState(null);
 
   async function importProject() {
     const filepath = await open({
@@ -30,6 +32,7 @@ export function StartScreen({ setDarkMode, toggleFullScreen, projects }) {
 
   return (
     <div className="start-screen">
+      {viewStartCard && <StartProject setProjectKey={setProjectKey} setDarkMode={setDarkMode} setViewStartCard={setViewStartCard} selectedProject={selectedProject} />}
       <div className="start-screen__title">Circles</div>
       <button
         className="start-screen__button"
@@ -43,44 +46,34 @@ export function StartScreen({ setDarkMode, toggleFullScreen, projects }) {
       >
         {translate("start_fullscreen_button")}
       </button>
-      <ProjectView projects={projects} setDarkMode={setDarkMode} />
+      <ProjectView projects={projects} setViewStartCard={setViewStartCard} setSelectedProject={setSelectedProject} />
     </div>
   );
 }
 
-function ProjectItem({ projectKey }) {
-  const [state, setState] = useState(STATES.idle);
-  const [error, setError] = useState(null);
+function ProjectView({ projects, setViewStartCard, setSelectedProject }) {
 
-  async function handleSubmit(e) {
-    e.preventDefault();
-    setState(STATES.working);
-    setError(null);
+  return (
+    <div className="start-screen__project-list">
+      <div className="start-screen__project-title">{translate("start_project_key")}</div>
+      <ul className="start-screen__project-list">
+        {projects.map((i, e) => (
+          <ProjectItem key={e} projectKey={i.key} setViewStartCard={setViewStartCard} setSelectedProject={setSelectedProject} />
+        )
+        )}
+      </ul>
+    </div>
+  );
+}
 
-    const data = new FormData(e.target);
-    const projectKey = data.get("projectKey");
-    const hostname = data.get("hostname");
-    const darkMode = data.get("darkMode");
-    try {
-      await invoke("select_project", { projectKey, hostname });
-      localStorage.setItem("circles.last_hostname", hostname);
-      setDarkMode(darkMode);
-      setProjectKey(projectKey);
-    } catch (error) {
-      setState(STATES.error);
-      setError(error);
-      // If an unknown error occurs, we want to log the details so we can see what went wrong
-      // eslint-disable-next-line no-console
-      if (error.kind === "Unknown") console.error(error);
-    }
-  }
-
+function ProjectItem({ projectKey, setViewStartCard, setSelectedProject }) {
   return (
     <li className="start-screen__project-item">
       <span className="project-item__title">{projectKey}</span>
-      <form className="project-item__controls">
+      <div
+        className="project-item__controls">
         <button
-          type="submit"
+          onClick={() => {setViewStartCard(true); setSelectedProject(projectKey);}}
           className="start-screen__button start-screen__button--start"
         >
           {translate("start_project_button")}
@@ -98,97 +91,107 @@ function ProjectItem({ projectKey }) {
         >
           {translate("start_delete_title")}
         </button>
-      </form>
+      </div>
     </li>
   );
 }
 
-function ProjectView({ projects, setDarkMode }) {
+function StartProject ({setProjectKey, selectedProject , setDarkMode, setViewStartCard }) {
+  const [state, setState] = useState(STATES.idle);
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setState(STATES.working);
+    setError(null);
+
+    const data = new FormData(e.target);
+    const projectKey = selectedProject;
+    const hostname = data.get("hostname");
+    const darkMode = data.get("darkMode");
+    try {
+      await invoke("select_project", { projectKey, hostname });
+      localStorage.setItem("circles.last_hostname", hostname);
+      setDarkMode(darkMode);
+      setProjectKey(projectKey);
+    } catch (error) {
+      setState(STATES.error);
+      setError(error);
+      // If an unknown error occurs, we want to log the details so we can see what went wrong
+      // eslint-disable-next-line no-console
+      if (error.kind === "Unknown") console.error(error);
+    }
+  }
 
   return (
-    <div className="start-screen__card">
-      <div className="start-screen__project-title">{translate("start_project_key")}</div>
-      <ul className="start-screen__project-list">
-        {projects.map((i, e) => (
-          <ProjectItem key={e} projectKey={i.key} />
-        )
-        )}
-      </ul>
-    </div>
+    <form
+      action=""
+      onSubmit={handleSubmit}
+      className="start-screen__card"
+      disabled={[STATES.working, STATES.done].includes(state)}
+    >
+      <div
+      className="start-screen__popup"
+      >
+      <h1 className="start-screen__title--dialog">
+        {translate("start_project_title")}
+      </h1>
+      <div className="start-screen__input">
+        <label className="start-screen__label" htmlFor="hostname">
+          {translate("start_reader_hostname")}
+        </label>
+        <input
+          className="start-screen__field"
+          type="text"
+          name="hostname"
+          id="hostname"
+          autoCapitalize="false"
+          placeholder="fx9600123456"
+          minLength={12}
+          maxLength={12}
+          required
+          defaultValue={previousHostname}
+        />
+      </div>
+      <div className="start-screen__input start-screen__input--checkbox">
+        <input
+          type="checkbox"
+          name="darkMode"
+          id="darkMode"
+          className="start-screen__checkbox"
+          defaultChecked={true}
+        />
+        <label htmlFor="darkMode" className="start-screen__label">
+          {translate("start_dark_mode")}
+        </label>
+      </div>
+      <div className="start-screen__button-container">
+      <button
+        type="submit"
+        className="start-screen__button"
+        disabled={[STATES.working, STATES.done].includes(state)}
+      >
+        {translate("start_project_button")}
+      </button>
+      <button
+        type="button"
+        className="start-screen__button"
+        onClick={() => setViewStartCard(false)}
+      >
+        {translate("cancel_button")}
+      </button>
+      </div>
+      {state === STATES.working && (
+        <span className="start-screen__message start-screen__message--spinner">
+          {translate("start_connecting")}
+        </span>
+      )}
+      {state === STATES.error && (
+        <span className="start-screen__message start-screen__message--error">
+          {translateError(error)}
+        </span>
+      )}
+      </div>
+    </form>
   );
 }
-
-// function ExportCard() {
-//   const [state, setState] = useState(STATES.error);
-//   const [error, setError] = useState(null);
-
-//   async function handleSubmit(e) {
-//     e.preventDefault();
-//     setState(STATES.working);
-
-//     const data = new FormData(e.target);
-//     const projectKey = data.get("projectKey");
-
-//     const filepath = await save({
-//       filters: [
-//         {
-//           name: "export",
-//           extensions: ["xlsx"],
-//         },
-//       ],
-//     });
-//     if (filepath === null) {
-//       setError(translate("start_export_no_filepath_error"));
-//       return;
-//     }
-
-//     try {
-//       await invoke("save_export", { filepath, projectKey });
-//       setState(STATES.done);
-//     } catch (e) {
-//       setError(e);
-//       setState(STATES.error);
-//     }
-//   }
-
-//   return (
-//     <form
-//       className="start-screen__card"
-//       onSubmit={handleSubmit}
-//       disabled={[STATES.working, STATES.done].includes(state)}
-//     >
-//       <h2 className="start-screen__title">{translate("start_export_title")}</h2>
-//       <div className="start-screen__input">
-//         <label className="start-screen__label" htmlFor="projectKey">
-//           {translate("start_project_key")}
-//         </label>
-//         <input
-//           className="start-screen__field"
-//           type="text"
-//           name="projectKey"
-//           id="projectKey"
-//           autoCapitalize="false"
-//           required
-//         />
-//       </div>
-//       <button className="start-screen__button" type="submit">
-//         {translate("start_export_button")}
-//       </button>
-//       {state === STATES.working && (
-//         <span className="start-screen__message start-screen__message--spinner">
-//           {translate("start_export_working")}
-//         </span>
-//       )}
-//       {state === STATES.done && (
-//         <span className="start-screen__message start-screen__message--success">
-//           {translate("start_export_done")}
-//         </span>
-//       )}
-//       {state === STATES.error && (
-//         <span className="start-screen__message start-screen__message--error">
-//           {error}
-//         </span>
-//       )}
-//     </form>
-//   );
-// }
