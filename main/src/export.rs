a684b9af152d629695e0010b48e5905a558db90e
Robbe Van Petegem <git@robbevp.be>
2025-05-02 17:29:46 +0200

    Rework tokens

--- main/src/export.rs
+++ main/src/export.rs
@@ -7,9 +7,9 @@ use crate::database::{
     schema::{answers, sessions, steps},
 };
 use std::collections::HashMap;
-use std::error::Error;
 
 const BATCH_SIZE: i64 = 10000;
+const TOKEN_LIST: &str = include_str!("../../data/tokens/list.json");
 
 pub fn export_project_data(
     connection: &mut SqliteConnection,
@@ -45,12 +45,14 @@ pub fn export_project_data(
     Ok(())
 }
 
-fn translate_token_key(input: &str) -> Result<String, Box<dyn Error>> {
-    let tokenlist_file_path = include_str!("../../data/tokens/list.json");
-    let tokens: HashMap<String, String> = serde_json::from_str(&tokenlist_file_path)?;
-    match tokens.get(input) {
-        Some(counterpart) => Ok(counterpart.clone()),
-        None => Ok(input.to_string()),
+fn tokens() -> HashMap<String, String> {
+    serde_json::from_str(TOKEN_LIST).unwrap()
+}
+
+fn get_token_type_from_key(input: &str) -> String {
+    match tokens().get(input) {
+        Some(counterpart) => counterpart.clone(),
+        None => input.to_string(),
     }
 }
 
@@ -82,7 +84,7 @@ fn fetch_batch_and_write(
         worksheet.write(row, 3, &step.question_key)?;
         worksheet.write_with_format(row, 4, created_at, &format)?;
         worksheet.write(row, 5, &answer.token_key)?;
-        worksheet.write(row, 6, translate_token_key(&answer.token_key).unwrap())?;
+        worksheet.write(row, 6, get_token_type_from_key(&answer.token_key))?;
         worksheet.write(row, 7, &answer.option_key)?;
     }
 
@@ -99,4 +101,22 @@ fn write_headers(worksheet: &mut Worksheet) -> Result<(), XlsxError> {
     worksheet.write(0, 6, "Token Identifier")?;
     worksheet.write(0, 7, "Option")?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_parse_token_list() {
+        // NOTE: We simply call this function to assert that this doesn't panic
+        tokens();
+    }
+
+    #[test]
+    fn should_find_token_from_key() {
+        let value = get_token_type_from_key("E2004702E4E16828021E62FE");
+
+        assert_eq!(value, "bl_bl");
+    }
 }
