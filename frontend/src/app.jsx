4067f507f0ff17d5f059215fc37dbdbe66e7693a
Robbe Van Petegem <git@robbevp.be>
2025-05-08 20:54:23 +0200

    Make sure we fetch projects when app renders

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -32,7 +32,7 @@ export default function App() {
     });
   }
 
-  useEffect(() => fetchProjects, []);
+  useEffect(() => fetchProjects(), []);
 
   useEffect(() => {
     document.addEventListener("keydown", handleKeydown);
